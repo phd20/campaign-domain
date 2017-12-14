edeffdbaf8c28bba1671046bd59d8f8cab490bd1
PhD20 <dmprofessor227@gmail.com>
2017-12-13 21:19:03 -0600

    Hello Campaign Domain
    Initial Commit:
    A functioning MVC WebApp with account confirmation and password recovery.

---
+++ campaigndomain/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace campaigndomain.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
